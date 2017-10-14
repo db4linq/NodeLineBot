85c9e084974427241899612672665f8254003933
db4linq <db4linq@gmail.com>
2017-10-14 21:35:23 +0700

    remove core

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
-app.use(cors())
 app.use(bodyParser.json())
-
 app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
